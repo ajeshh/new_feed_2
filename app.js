566bf01227e363436478ef91f34b329c59ff51fe
Ajesh Shah <ajeshshah@gmail.com>
2014-12-05 16:09:11 -0800

    ejs layout

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var express = require("express"),
+  bodyParser = require("body-parser"),
+  methodOverride = require("method-override"),
+  pg = require("pg"),
   app = express();
 
-//ejs
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+
+app.get("/articles", function (req, res) {
+res.render("articles/index");
+});
+
+app.get("/articles/new", function (req, res) {
+res.render("articles/new");
+});
 
 
 //port listening
