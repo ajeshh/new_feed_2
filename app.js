45bb843f6cebede9d7c568878b6486c28bd14ea0
Ajesh Shah <ajeshshah@gmail.com>
2014-12-06 13:26:35 -0800

    db creation and validation

--- app.js
+++ app.js
@@ -7,14 +7,35 @@ var express = require("express"),
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var config = {
+  database: "articles_app",
+  port: 5432,
+  host:"localhost"
+}; 
+
 
 app.get("/articles", function (req, res) {
-res.render("articles/index");
+   pg.connect(config, function(err, client, done){
+        if (err) {
+             console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
+        }
+        client.query("SELECT * FROM articles", function (err, result) {
+            done(); 
+            console.log(result.rows);  
+            res.render("articles/index", {articleList: result.rows});         
+        });
+
+    });
 });
+
+
+
 
 app.get("/articles/new", function (req, res) {
 res.render("articles/new");
 });
+
+
 
 
 //port listening
